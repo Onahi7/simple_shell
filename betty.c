70149cea14d8abcb11e3cc6496b72eda73ce2645
Onahi7 <dicksonhardy7@gmail.com>
2023-09-21 04:02:21 -0700

    hello

--- betty.c
+++ betty.c
@@ -1,18 +1,28 @@
-#include "main.h"
-#include <stdio.h>
+/**
+ * print_hello - Prints "Hello, Betty!" to the standard output.
+ *
+ * This function prints a greeting message to the standard output.
+ */
+void print_hello(void)
+{
+        char *message = "Hello, Betty!";
+
+        /* Print the message */
+        printf("%s\n", message);
+}
 
 /**
- * main - check the code 
+ * main - Entry point of the program.
  *
- * Return: Always 0.
+ * This is the main function of the program. It calls the print_hello
+ * function to print a greeting message.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int n;
+	/* Call the print_hello function */
+	print_hello();
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+	return (0);
 }

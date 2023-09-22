02bb02932d3cdd124c5649f4fc86f20615b1f714
Enosuji <enoosuji@gmail.com>
2023-09-23 00:09:49 +0100

    Update shell_exit.c

--- shell_exit.c
+++ shell_exit.c
@@ -19,14 +19,14 @@ int shell_exit(char **arguments, inform *state, char **argv)
 
 	if (arguments[1])
 	{
-		exit_status = _atoi(arguments[1]);
+		exit_status = string_to_int(arguments[1]);
 		if (exit_status == -1)
 		{
-			write(2, argv[0], _strlen(argv[0]));
+			write(2, argv[0], str_size(argv[0]));
 			write(2, ": 1: ", 5);
-			write(2, arguments[0], _strlen(arguments[0]));
+			write(2, arguments[0], str_size(arguments[0]));
 			write(2, ": Illegal number: ", 18);
-			write(2, arguments[1], _strlen(arguments[1]));
+			write(2, arguments[1], str_size(arguments[1]));
 			write(2, "\n", 1);
 			state->status = 2;
 			return (1);

09a6febfd0ef3be3c9086854e8def8c2d49a1a03
Enosuji <enoosuji@gmail.com>
2023-09-23 08:26:56 +0100

    Update interactive.c

--- interactive.c
+++ interactive.c
@@ -1,9 +1,14 @@
 #include "shell.h"
 
 /**
- * inter: check for interactive
+ * interactive - Check if the shell is running in interactive mode.
  *
- * Return: 1 or 0.
+ * This function checks whether the shell is running in interactive mode, which
+ * is typically determined by whether it is connected to a terminal.
+ *
+ * Return:
+ *   - 1 if running in interactive mode (connected to a terminal).
+ *   - 0 if not running in interactive mode (not connected to a terminal).
  */
 int inter(void)
 {

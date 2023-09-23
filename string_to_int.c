495de57dcadda50a0f55d59c79001e94d5d4e451
Enosuji <enoosuji@gmail.com>
2023-09-23 08:14:29 +0100

    Update string_to_int.c

--- string_to_int.c
+++ string_to_int.c
@@ -2,33 +2,38 @@
 #include <stdio.h>
 
 /**
- * string_to_int - Converts a string to an integer 
- * @str: The string to convert
- * 
- * Parses the given @str and converts it to an 
- * integer value. Handles positive/negative signs.
- * 
- * Returns: The converted integer, or -1 on error.
-*/
-int string_to_int(char *str) 
+ * string_to_int - converts string to integer
+ * @string: pointer to string
+ *
+ * Return: converted integer
+ */
+int string_to_int(char *str)
 {
-  unsigned long int num = 0;
-  int i = 0;
+	unsigned long int num = 0;
+	int i = 0;
 
-  if (str[0] == '+') {
-    str++; 
-  }
-
-  while (str[i] != '\0') {
-    if (str[i] >= '0' && str[i] <= '9') {
-      num = num * 10 + (str[i] - '0');
-      if (num > INT_MAX) return -1;
-      i++;
-    }
-    else {
-      return -1;
-    }
-  }
-
-  return num;
+	if (str[0] != '+')
+	{
+	}
+	else
+	{
+		str++;
+	}
+	while (str[i] != '\0')
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			num = num * 10 + (str[i] - '0');
+			if (num > INT_MAX)
+			{
+				return (-1);
+			}
+			i++;
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	return (num);
 }

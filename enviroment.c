cee69a829ad328acb8e50176c1fe4abefe523198
Enosuji <enoosuji@gmail.com>
2023-09-23 02:15:17 +0100

    Update enviroment.c

--- enviroment.c
+++ enviroment.c
@@ -1,22 +1,32 @@
 #include <string.h>
 #include "shell.h"
 
-
 /**
  * enviroment_print - this function displays environment variables
  * @arguments: argument
- * Return: nothing
+ * Return: 0 on success, -1 on failure
  */
-
 int enviroment_print(char **arguments)
 {
-	char **env = environ;
+    char **env = environ;
+    (void)arguments;
 
-	(void)arguments;
-	for (; *env != NULL; env++)
-	{
-		write(1, *env, strlen(*env));
-		write(1, "\n", 1);
-	}
-	return (0);
+    for (; *env != NULL; env++)
+    {
+        /* Print each environment variable */
+        if (write(1, *env, strlen(*env)) == -1)
+        {
+            perror("write");
+            return -1;
+        }
+
+        if (write(1, "\n", 1) == -1)
+        {
+            perror("write");
+            return -1;
+        }
+    }
+
+    return 0;
 }
+
